dddb796751b27e05ea7ac8e8ed28f8f1c2a87f12
ashwani8418 <68030476+ashwani8418@users.noreply.github.com>
2023-06-23 21:31:42 +0530

    Bird made abstract and instantiated

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import org.example.Bird.Bird;
+import org.example.panchi.Eagle;
+import org.example.panchi.Sparrow;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Bird eagle = new Eagle ("Bhagwa Color", 10.9, "Horn-billed");
+        eagle.fly ();
+        Bird sparrow = new Sparrow ("Bhagwa Color", 9.0, "Horn");
+        sparrow.fly ();
     }
 }
--- src/main/java/org/example/Bird/Bird.java
+++ src/main/java/org/example/Bird/Bird.java
@@ -1,18 +1,13 @@
 package org.example.Bird;
 
-public class Bird {
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public abstract  class  Bird {
 
     private String color;
     private Double weight;
-    private BirdType type;
     private String beakType;
 
-    public void fly(){
-        if(type == BirdType.EAGLE){
-            System.out.println ("Eagle is flying");
-        }
-        else if(type == BirdType.SPARROW){
-            System.out.println ("Sparrow is flying");
-        }
-    }
+    public abstract void fly();
 }
