e77147bf1563fe11b31dc68a92f712be5762e504
ashwani8418 <68030476+ashwani8418@users.noreply.github.com>
2023-06-23 23:41:35 +0530

    Flyable interface created to implement another make the code loosely coupled

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,7 +2,10 @@ package org.example;
 
 import org.example.Bird.Bird;
 import org.example.panchi.Eagle;
+import org.example.panchi.Interfaces.Flyable;
 import org.example.panchi.Sparrow;
+
+import java.util.List;
 
 /**
  * Hello world!
@@ -12,9 +15,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        Bird eagle = new Eagle ("Bhagwa Color", 10.9, "Horn-billed");
-        eagle.fly ();
-        Bird sparrow = new Sparrow ("Bhagwa Color", 9.0, "Horn");
-        sparrow.fly ();
+        Eagle eagle = new Eagle ("Bhagwa Color", 10.9, "Horn-billed");
+        Sparrow sparrow = new Sparrow ("Bhagwa Color", 9.0, "Horn");
+        List<Flyable> flyableBird = List.of (eagle, sparrow);
+        flyableBird.forEach (Flyable::fly);
     }
 }
--- src/main/java/org/example/Bird/Bird.java
+++ src/main/java/org/example/Bird/Bird.java
@@ -3,11 +3,10 @@ package org.example.Bird;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-public abstract  class  Bird {
+public abstract  class  Bird{
 
     private String color;
     private Double weight;
     private String beakType;
 
-    public abstract void fly();
 }
